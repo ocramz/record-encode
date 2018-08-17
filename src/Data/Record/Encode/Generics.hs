fb6e4ad0b5a6649c51516b932885eb3108e12b9b
ocramz <zocca.marco@gmail.com>
2018-08-17 22:46:29 +0200

    add GEncode

--- src/Data/Record/Encode/Generics.hs
+++ src/Data/Record/Encode/Generics.hs
@@ -1,6 +1,7 @@
 {-# language DeriveGeneric #-}
 {-# language TypeFamilies #-}
 {-# language TypeOperators #-}
+{-# language MultiParamTypeClasses #-}
 module Data.Record.Encode.Generics where
 
 import GHC.Generics
@@ -8,21 +9,46 @@ import GHC.Generics
 import qualified Data.Vector as V
 
 
+
+
+class GEncode i o x where
+  gencode :: i x -> Maybe (o x)
+
+-- λ> :t fmap to . gencode . from
+-- 
+-- fmap to . gencode . from
+--   :: (GEncode (Rep a) (Rep b) x, Generic b, Generic a) =>
+--      a -> Maybe b
+
+
+-- instance GEncode (V1 p) where
+--   gencode _ = error "Cannot encode V1"
+
+-- instance GEncode (U1 p) where
+--   gencode U1 = error "Cannot encode U1"
+
+
+
+
+
+
+
 class Encode d where
   type ETy d :: *
-  encode :: d -> V.Vector (ETy d)
+  encode :: d -> V.Vector (ETy d)  
 
-instance Encode (V1 p) where
-  encode _ = error "Cannot encode V1"
+-- instance Encode (V1 p) where
+--   encode _ = error "Cannot encode V1"
 
-instance Encode (U1 p) where
-  encode U1 = error "Cannot encode U1"
+-- instance Encode (U1 p) where
+--   encode U1 = error "Cannot encode U1"
 
-instance Encode (K1 i c p) where
+-- instance Encode (K1 i c p) where
 
-instance Encode (M1 i c f p) where
+-- instance Encode (M1 i c f p) where
 
-instance Encode ((f :*: g) p) where
+-- instance Encode ((f :*: g) p) where
 
-instance Encode ((f :+: g) p) where    
+-- instance Encode ((f :+: g) p) where  
+  
 
